f5eb9c0d0485a698eb6266cee2d03c1e510878b8
Sorin Gore <soringoredev@gmail.com>
2026-01-28 22:17:44 +0200

    Add changes to test

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("test")
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def start_engine(self):
+            print(f"The {self.color} {self.brand} is starting!")
+
+
+car1 = Car(brand='Toyota', color='red')
+car2 = Car(brand='Honda', color='blue')
+
+
+print(car1.brand)
+print(car2.color)
